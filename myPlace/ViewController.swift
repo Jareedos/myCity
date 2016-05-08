5e3fb4ed7e43b49f41659fe75427b783dffdd184
Jareedos <jaredallens@gmail.com>
2016-05-08 13:26:13 -0700

    UI for first View Controller complete

---
+++ myPlace/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  myPlace
+//
+//  Created by Jared Sobol on 5/8/16.
+//  Copyright © 2016 Appmaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var moreBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
